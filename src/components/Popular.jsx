b9c28a72488a07f3de0751f78cc90d88b0806b40
Rachel Solov <rsolov23@yahoo.com>
2022-07-16 16:56:44 -0500

    added local storage

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
 import config from "../config";
+import { Wrapper, Card, Gradient } from "../Styles/StyledPopular";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
 
 function Popular() {
   const [popular, setPopular] = useState([]);
@@ -9,22 +12,48 @@ function Popular() {
   }, []);
 
   const getPopular = async () => {
-    const key = config.REACT_APP_API_KEY;
-    const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${key}&number=9`
-    );
-    const data = await api.json();
-    setPopular(data.recipes);
+    const check = localStorage.getItem("popular");
+
+    if (check) {
+      setPopular(JSON.parse(check));
+    } else {
+      const key = config.REACT_APP_API_KEY;
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${key}&number=9`
+      );
+      const data = await api.json();
+
+      localStorage.setItem("popular", JSON.stringify(data.recipes));
+      setPopular(data.recipes);
+      console.log(data.recipes);
+    }
   };
   return (
     <div>
-      {popular.map((recipe) => {
-        return (
-          <div key={recipe.id}>
-            <p>{recipe.title}</p>
-          </div>
-        );
-      })}
+      <Wrapper>
+        <h3>Popular Picks</h3>
+        <Splide
+          options={{
+            perPage: 4,
+            arrows: false,
+            pagination: false,
+            drag: "free",
+            gap: "5rem",
+          }}
+        >
+          {popular.map((recipe) => {
+            return (
+              <SplideSlide key={recipe.id}>
+                <Card>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt="recipe-title" />
+                  <Gradient />
+                </Card>
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+      </Wrapper>
     </div>
   );
 }
